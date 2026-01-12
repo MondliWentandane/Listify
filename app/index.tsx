cba8aeb054720eb4ed75995b2dec03b6b8c56738
Mondli <mondli46401@gmail.com>
2026-01-12 16:00:09 +0200

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import BackgroundComp from '@/components/BackgroundComp';
+import React from 'react'
+import { StyleSheet, Text } from 'react-native';
+
+const index: React.FC = () => {
+  return (
+    <BackgroundComp>
+
+    </BackgroundComp>
+  )
+}
+
+export default index;
+
+const styles= StyleSheet.create({
+  
+})
---
+++ components/BackgroundComp.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet, View, ViewProps, ViewStyle } from 'react-native'
+
+//Creating an interface that will hold the props for this comp
+interface TheProps extends ViewProps{
+    children?: React.ReactNode;
+    style?: ViewStyle | ViewStyle[];
+}
+
+const BackgroundComp: React.FC<TheProps> = ({children, style}) => {
+  return (
+    <View style={[styles.container, style]}>
+        {children}
+    </View>
+  )
+}
+
+export default BackgroundComp;
+
+const styles= StyleSheet.create({
+    container:{
+        backgroundColor:"#e3e3ec",
+        width: "100%",
+        height:"94%"
+    }
+});
